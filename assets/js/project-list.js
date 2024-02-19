66d1e3c7a55203543bc9eacff3d8bae20c0e4746
nachodlv <ignacio.delavega@ing.austral.edu.ar>
2024-02-18 22:15:06 -0300

    Add new projects and add functionality

---
+++ assets/js/project-list.js
@@ -0,0 +1,103 @@
+﻿const psychoPetting = {
+    title: "Psycho Petting",
+    link: "https://gitlab.com/nachodlv/ld51",
+    video: "https://www.youtube.com/embed/giFF3UcmlzU",
+    description: [
+        "This game was made in the last Ludum Dare Jam 51 where I got the chance to work with my friends for 48 hours to " +
+        "make a fun game. The Sinopsis is: Is it a dream? Is it another planet? Is it a looped nightmare as a result of a " +
+        "bad trip? We’ll never know, what we do know is that you have to survive the terrible nights and enjoy the quiet " +
+        "days while you can",
+        "Implemented some abilities of the main character in single-player games using GAS. Implementation of different UI " +
+        "interfaces using the Unreal Engine widget system & development of the behaviour of the AIs. Apart from unreal engine 5, " +
+        "we used mural, jira and perforce to manage our workspace."
+    ]
+}
+
+const nimble_test = {
+    title: "Nimble Technical Test",
+    video: "https://www.youtube.com/embed/nYIcjOwMbQU",
+    description: [
+        "In this project the players have to escape a broken spaceship that landed on an stranded frozen planet by " +
+        "shooting the walls that are blocking the exit. They'll have to be a team to make it out. ",
+        "It's a multiplayer shooter game with a pyramid of random cubes that can be destroyed if hit. Similar to tetris " +
+        "where the player needs to hit the same rows of colors.",
+        "While making this project I learned how to use implement GAS to add abilities to the player for example the " +
+        "Leaning Ability and modify the networking of such using replicated flags in the character movement component. " +
+        "The characters can take damage using niagara for blood and ragdoll."
+    ]
+}
+
+const mila_shooter = {
+    title: "Mila Shooter",
+    video: "https://www.youtube.com/embed/VbktyXqkxUw",
+    link: "https://gitlab.com/milapesa/milasshooter",
+    description: [
+        "This was a project I made through a course to learn the basics of unreal engine. In this first approach I " +
+        "learned how collisions work, widgets, animation state machines, sound events and even behavior trees, " +
+        "different senses for the AI.",
+        "It's a first person game for PC, I made level design and basic mechanics of a First Person Shooter (character movement), " +
+        "UI, defeat and victory condition. I programmed and configured an AI for the game. Finally I also provided " +
+        "feedback to the player through sounds and UI effects"
+    ]
+}
+
+const adoptar_mascota = {
+    title: "Adoptar Mascota",
+    video: "https://www.youtube.com/embed/ITbivnRuGAk?si=dTWaR-m4b-c-Loiq",
+    description: [
+        "This project was made for as an university assingment to create a working website for a bussiness. We decided " +
+        "to develop a Adoption System for users and administrators to add pets to the website and manage their info as " +
+        "well as the adoption process. We used Node.js, React & Bootstrap for functionality and style."
+    ]
+}
+
+const climbing_system = {
+    title: "Climbing System",
+    video: "https://www.youtube.com/embed/1Xgvaq0piO0?si=q1648zOMHF428qiq",
+    description: [
+        "This project was inspired by the Netflix show \"Blue Eye Samurai\", I wanted to replicate the action of the " +
+        "show in Unreal Engine.",
+        "I'm currently developing the aesthetic of the level to resemble the Japanese environment. I created a climbing " +
+        "system for the character where the player can climb surfaces in all directions, hop while climbing, detect ledges, " +
+        "trigger animations as well as vault through objects.",
+        "It was made using C++ and I learned how to implement animation, use control rig to adjust the hands & feet, " +
+        "adjust them in the Level Sequence, create montages for the different abilities, take advantage of the enhanced " +
+        "input for the hop action and use multiple ray traces to iterate the different surfaces."
+    ]
+}
+
+const game_dev_projects = [
+    climbing_system,
+    adoptar_mascota,
+    psychoPetting,
+    nimble_test,
+    mila_shooter
+];
+
+const web_projects = [
+    adoptar_mascota,
+    climbing_system,
+    psychoPetting,
+    nimble_test,
+    mila_shooter
+]
+
+
+const about_me_game_dev = {
+    job_title: "Gameplay Developer C++",
+    job_description: "Passion for games",
+    resume: "assets/documents/Mila_Pesa_Mazzola-Game_Programmer-CV.pdf",
+    description: [
+        `I’m a Game Programmer with a robust background in C++ development, 2 years of experience with Unreal Engine 4 & 5, and
+        a passion for creating new gameplay mechanics. With a proven track record, I've successfully collaborated in dynamic teams,
+        assuming roles such as Gameplay Developer, Front-End Specialist, Console Porting Expert, and Quality Assurance professional.`
+    ]
+}
+
+const about_me_web = {
+    job_title: "Software Developer",
+    resume: "assets/documents/Mila_Pesa_Mazzola-Web-CV.pdf",
+    description: [` I’m a Software Developer with a robust background in  Node, React & C++ development. With a proven
+        track record, I've successfully collaborated in dynamic  teams, assuming roles such as Front-End Specialist, Game
+        Developer, Console Porting Expert, and Quality Assurance professional`]
+}
---
+++ assets/js/project-utils.js
@@ -0,0 +1,80 @@
+﻿function getCarousel(images) {
+    const imagesHTML = images.map(image => `<div class="carousel-item"><img class="d-block w-100" src="${image}" alt="Slide"></div>`).join("");
+
+    return `
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner"> ${imagesHTML} </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    `;
+}
+
+function getProjectVideoAsHTML(project) {
+    return `
+        <a href="#" class="row project-video">
+            <iframe class="image fit" src="${project.video}" alt="" allow="fullscreen;" style="max-width:700px; height: 400px;"></iframe>
+        </a>
+    `;
+}
+function getProjectAsHTML(project) {
+    
+    const projectTitle = `
+        ${project.link? `<a href="${project.link}">` : ""}
+            <h3>${project.title}</h3>
+        ${project.link? `</a>` : ""}
+    `;
+    
+    let paragraphs = "";
+    for (let paragraph of project.description) {
+        paragraphs += `<p>${paragraph}</p>`;
+    }
+    
+    return `
+        ${projectTitle}
+        <div class = "row">
+            <div class = col-12>
+                ${project.video ? getProjectVideoAsHTML(project) : ""}
+            </div>
+            <div class="inner">${paragraphs}</div>
+        </div>
+    `
+}
+
+function constructProjectList(projectList) {
+    let projectsAsHTML = "";
+    for (let project of projectList) {
+        projectsAsHTML  += `<div>${getProjectAsHTML(project)}</div>`;
+    }
+    document.getElementById("project-list").innerHTML += projectsAsHTML;
+}
+
+function build_about_me(about_me) {
+    document.getElementById("resume").href = about_me.resume;
+    document.getElementById("job-title").innerHTML = about_me.job_title;
+    document.getElementById("job-description").innerHTML = about_me.job_description ? about_me.job_description : "";
+    document.getElementById("about-me").innerHTML= about_me.description.map(description => `<p>${description}</p>`).join("");
+}
+
+function main() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const isWeb = urlParams.has('web');
+
+    constructProjectList(isWeb ? web_projects : game_dev_projects);
+
+    build_about_me(isWeb ? about_me_web : about_me_game_dev);
+}
+
+main();
